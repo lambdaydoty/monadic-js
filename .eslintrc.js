a48100478d073d350a922966454de67fd6301ff0
lambdaydoty <euphrates.tigris@gmail.com>
2020-11-01 15:35:38 +0000

    Add .eslint jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,12 +6,21 @@ module.exports = {
   ],
   "plugins": [
     "no-only-tests",
-    "flowtype"
+    "flowtype",
+    "jest"
   ],
   "rules": {
     "comma-dangle": ["error", "always-multiline"],
     "no-only-tests/no-only-tests": "error",
     "func-call-spacing": ["error", "always"],
-    "no-multi-spaces": ["error", { "ignoreEOLComments": true }]
+    "no-multi-spaces": ["error", { "ignoreEOLComments": true }],
+    "jest/no-disabled-tests": "warn",
+    "jest/no-focused-tests": "error",
+    "jest/no-identical-title": "error",
+    "jest/prefer-to-have-length": "warn",
+    "jest/valid-expect": "error"
   },
+  "env": {
+    "jest/globals": true
+  }
 }
