196de08386ec07a22d5c81a22266be23522127d7
lambdaydoty <euphrates.tigris@gmail.com>
2020-11-01 16:02:20 +0000

    Upgrade test to fluture

--- src/utils/index.js
+++ src/utils/index.js
@@ -5,6 +5,7 @@ const [F, FN] = [
 
 module.exports = {
   get,
+  rand,
 }
 
 function get (url, timeout = 800) {
@@ -14,4 +15,11 @@ function get (url, timeout = 800) {
     .pipe (F.chain (FN.bufferResponse ('utf8')))          // ∷ Future Error String
     .pipe (F.chain (F.encase (JSON.parse)))               // ∷ Future Error Json
     .pipe (F.race (F.rejectAfter (timeout) (new Error (`*timeou(${timeout})t*`))))
+}
+
+function rand (bits = 32 * 8) {
+  const crypto = require ('crypto')
+  return crypto
+    .randomBytes (Math.ceil (bits / 8))
+    .toString ('hex')
 }
--- test/api/addresses.test.js
+++ test/api/addresses.test.js
@@ -3,6 +3,7 @@ const { MONGO_URL } = process.env
 const models = require ('../../src/models')
 const request = require ('supertest')
 const { create } = require ('../../src/app')
+const { rand } = require ('../../src/utils')
 
 let client = null
 
@@ -19,26 +20,22 @@ afterAll (async () => {
 
 const F = require ('fluture')
 
-it ('should insert a doc', async () => {
-  const currency = 'xyz'
-  const user = 'test'
-  const token = 'qwer1234'
+const asyncTest = (name, routine) => test (name, () => F.promise (F.go (routine)))
+// const control = F.coalesce (S.Left) (S.Right)
+
+asyncTest ('Get an address', function * () {
+  const [currency, user, token, address] = [rand (), rand (), rand (), rand ()]
   const { Account, Address, Currency } = models (client) (currency)
-  const mock = { currency, account: user, address: 'bar', client_id: null }
+  const mock = { currency, account: user, address, client_id: null }
 
-  await Currency
-    .insertOne ({ _id: currency })
-    .pipe (F.promise)
-  await Account
-    .insertOne ({ _id: user, token })
-    .pipe (F.promise)
-  await Address
-    .insertOne (mock)
-    .pipe (F.promise)
+  yield Currency.insertOne ({ _id: currency })
+  yield Account.insertOne ({ _id: user, token })
+  yield Address.insertOne (mock)
 
-  const { status, body } = await request (create (client))
+  const { status, body } = yield F.node (done => request (create (client))
     .get (`/api/${user}/${currency}/addresses/bar`)
     .set ('token', token)
+    .end (done))
 
   expect (status).toBe (200)
   expect (body).toMatchObject (mock)
