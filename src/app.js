19294044050eee2d5cc9c0ad1537c1df94f1e34a
lambdaydoty <euphrates.tigris@gmail.com>
2020-12-11 02:46:17 +0000

    Put locals for momi state

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
-const [F, { middleware, Next }] = [
-  require ('fluture'),
-  require ('fluture-express'),
-]
+const F = require ('fluture')
+const L = require ('partial.lenses')
+const { middleware, Next } = require ('fluture-express')
+const { unchecked: S } = require ('sanctuary')
+
 const express = require ('express')
 const { catchall } = errorHandlers ()
 const { NODE_ENV } = process.env
@@ -30,11 +31,19 @@ const custom = {
   },
 }
 
-const { go, connect, get, put, lift } = require ('momi')
+const LOCALS = ['locals', L.valueOr ({})]
+const { go, connect, get, put, lift, modify } = require ('momi')
 // connect ∷ Middleware → ((Req, Res, Next) → Unit)
 // go ∷ GeneratorFunction → Middleware
+// Middleware = StateT[Future a b] → StateT[Future c d]
 
 const { header, validationResult } = require ('express-validator')
+
+function putLocals (req, res, next) {
+  const { locals } = res
+  Object.assign (req, { locals })
+  next ()
+}
 
 const fromValidator = validator => go (function * (next) {
   const req = yield get
@@ -52,10 +61,8 @@ const queryValidator = fromValidator (
 const validateAll = go (function * (next) {
   const { BadHeader } = require ('./errors')
   const req = yield get
-  const result = validationResult (req)
-    .formatWith (({ location, msg, param }) => {
-      return '' + location + ' ' + param + ': ' + msg
-    })
+  const formatter = ({ location, msg, param }) => `${location} ${param}: ${msg}`
+  const result = validationResult (req).formatWith (formatter)
   if (!result.isEmpty ()) yield lift (F.reject (new BadHeader (`${result.array ()}`)))
   return yield next
 })
@@ -63,21 +70,20 @@ const validateAll = go (function * (next) {
 const queryParser = go (function * (next) {
   const req = yield get
   const token = req.get ('X-Token')
-  yield put (Object.assign ({ token }, req))
+  yield modify (L.modify (LOCALS) (S.concat ({ token })))
   return yield next
 })
 
 const echo = go (function * (/* next */) {
-  const { token } = yield get
+  const { locals: { token, client } } = yield get
   console.log (yield lift (F.after (10) ('@echo')))
+  console.log (`connection: ${client.isConnected ()}`)
   return {
     status: 200,
     headers: { 'X-Powered-By': 'momi' },
     body: `Hello from momi (token:${token})\n`,
   }
 })
-
-const { unchecked: S } = require ('sanctuary')
 
 module.exports = {
   create (client/* , keychain */) {
@@ -86,7 +92,7 @@ module.exports = {
       .use (express.json ({ verify: saveraw }))
       .use (custom.saveclient (client))
       .use (custom.body ())
-      .get ('/momi', connect (S.pipe ([echo, queryParser, validateAll, queryValidator])))
+      .get ('/momi', [putLocals, connect (S.pipe ([echo, queryParser, validateAll, queryValidator]))])
       .use (require ('./routes'))
       .use (catchall)
     return app
